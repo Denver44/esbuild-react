4330edbbaaa8312cd0ac015c772add2660401564
Denver <70090559+Denver44@users.noreply.github.com>
2022-02-09 13:18:04 +0530

    basic from element added

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,21 @@
+import { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  return <h1>hi</h1>;
+  const [code, setCode] = useState('');
+  const [input, setInput] = useState('');
+  const handleSubmit = () => {
+    console.log(input);
+  };
+  return (
+    <div>
+      <textarea onChange={(e) => setInput(e.target.value)}></textarea>
+      <div>
+        <button onClick={handleSubmit}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
