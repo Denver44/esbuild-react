58d9dc54c09ebeb3a8a2b7fb7294540d2e22342d
Denver <70090559+Denver44@users.noreply.github.com>
2022-02-09 13:55:58 +0530

    use ref for arbitary value

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,21 @@
 import * as esBuild from 'esbuild-wasm';
 import ReactDOM from 'react-dom';
-import { useState, useEffect } from 'react';
-
-const startService = async () => {
-  const service = await esBuild.startService({
-    worker: true,
-    wasmURL: '/esbuild.wasm',
-  });
-  console.log(service);
-};
+import { useState, useEffect, useRef } from 'react';
 
 const App = () => {
+  const ref = useRef<any>();
   const [code, setCode] = useState('');
   const [input, setInput] = useState('');
   const handleSubmit = () => {
-    console.log(input);
+    if (!ref.current) return;
+    console.log(ref);
   };
-
+  const startService = async () => {
+    ref.current = await esBuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm',
+    });
+  };
   useEffect(() => {
     startService();
   }, []);
