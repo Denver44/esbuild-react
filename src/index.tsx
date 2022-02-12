aa14353e27829617e95270fb6b7fd3e9fb86d89a
Denver <70090559+Denver44@users.noreply.github.com>
2022-02-12 19:03:13 +0530

    small issue warngings  fix

--- src/index.tsx
+++ src/index.tsx
@@ -11,10 +11,34 @@ const startService = async (ref: React.MutableRefObject<any>) => {
   });
 };
 
+const html = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  </head>
+  <body>
+    <div id="root"></div>
+    <script>
+    window.addEventListener("message", (event) => {
+      try {
+        eval(event.data);
+      } catch (error) {
+        const root = document.querySelector('#root');
+        const errorMsg = '<div style="color: red;"><h4>Runtime Error : </h4>' + error + '</div>';
+        root.innerHTML = errorMsg;
+        console.error(error);
+      }
+    }, false);
+    </script>
+  </body>
+</html>
+`;
+
 const App = () => {
   const ref = useRef<any>();
   const iFrame = useRef<any>();
-  const [code, setCode] = useState<string>('');
+
+  const [code] = useState<string>('');
   const [input, setInput] = useState('');
 
   useEffect(() => {
@@ -23,7 +47,8 @@ const App = () => {
 
   const handleSubmit = async () => {
     if (!ref.current) return;
-    // transform function actually does the transpiling for us.
+
+    iFrame.current.srcdoc = html;
     const res = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
@@ -34,35 +59,9 @@ const App = () => {
         global: 'window',
       },
     });
-
-    // console.log(res);
-    console.log('iFrame ', iFrame);
     iFrame.current.contentWindow.postMessage(res.outputFiles[0].text, '*');
   };
 
-  const html = `
-  <!DOCTYPE html>
-  <html lang="en">
-    <head>
-    </head>
-    <body>
-      <div id="root"></div>
-      <script>
-      window.addEventListener("message", (event) => {
-        try {
-          eval(event.data);
-        } catch (error) {
-          const root = document.querySelector('#root');
-          const errorMsg = '<div style="color: red;"><h4>Runtime Error : </h4>' + error + '</div>';
-          root.innerHTML = errorMsg;
-          console.error(error);
-        }
-      }, false);
-      </script>
-    </body>
-  </html>
-  
-  `;
   return (
     <div>
       <textarea onChange={(e) => setInput(e.target.value)}></textarea>
@@ -70,10 +69,9 @@ const App = () => {
         <button onClick={handleSubmit}>Submit</button>
       </div>
       <pre>{code}</pre>
-      {/* We told the iframe that please allow the script tag in the iframe */}
       <iframe
         ref={iFrame}
-        title="test-frame"
+        title="preview"
         sandbox="allow-scripts"
         srcDoc={html}
       />
