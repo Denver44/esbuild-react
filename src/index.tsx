41cd7383415a55426b674dc39121f7e62ed54ae8
Denver <70090559+Denver44@users.noreply.github.com>
2022-02-14 15:28:05 +0530

    Multiple Editors and Preview Windows done

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,11 @@
-import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import ReactDOM from 'react-dom';
-import { useState } from 'react';
-import CodeEditor from './components/CodeEditor';
-import Preview from './components/Preview';
-import bundle from './bundle';
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
+import CodeCell from './components/CodeCell';
 
 const App = () => {
-  const [code, setCode] = useState<string>('');
-  const [input, setInput] = useState('');
-
-  const handleSubmit = async () => {
-    const result = await bundle(input);
-    setCode(result);
-  };
-
   return (
     <div>
-      <CodeEditor
-        initialValue="const a = 1;"
-        onChange={(value) => setInput(value)}
-      />
-      <div>
-        <button onClick={handleSubmit}>Submit</button>
-      </div>
-      <Preview code={code} />
+      <CodeCell />
     </div>
   );
 };
