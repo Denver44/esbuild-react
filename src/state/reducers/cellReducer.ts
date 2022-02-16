2e0e176ee11755c1de7c604efe4a7c3959042cfd
Denver <70090559+Denver44@users.noreply.github.com>
2022-02-17 01:18:18 +0530

    swapping cell reducer done

--- src/state/reducers/cellReducer.ts
+++ src/state/reducers/cellReducer.ts
@@ -2,6 +2,8 @@ import { ActionType } from '../action-types';
 import { Action } from '../actions';
 import { Cell } from '../cell';
 import produce from 'immer';
+import { stat } from 'fs';
+import { act } from 'react-dom/test-utils';
 
 interface CellsState {
   loading: boolean;
@@ -21,7 +23,6 @@ const initialState: CellsState = {
 
 const reducer = produce((state: CellsState = initialState, action: Action) => {
   switch (action.type) {
-    
     case ActionType.UPDATE_CELL:
       const { id, content } = action.payload;
       state.data[id].content = content;
@@ -34,8 +35,17 @@ const reducer = produce((state: CellsState = initialState, action: Action) => {
 
     case ActionType.INSERT_CELL_BEFORE:
       return state;
+
     case ActionType.MOVE_CELL:
-      return state;
+      const { direction } = action.payload;
+      const index = state.order.findIndex((id) => id === action.payload.id);
+      const newIndex = direction === 'up' ? index - 1 : index + 1;
+      if (newIndex < 0 || newIndex > state.order?.length - 1) return;
+
+      state.order[index] = state.order[newIndex];
+      state.order[newIndex] = action.payload.id;
+      return;
+
     default:
       return state;
   }
