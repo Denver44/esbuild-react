8ef3969224a9e9d2194e786e0b10a0e633be7966
Denver <70090559+Denver44@users.noreply.github.com>
2022-02-17 09:50:02 +0530

    ts Gotcha Fix of immer

--- src/state/reducers/cellReducer.ts
+++ src/state/reducers/cellReducer.ts
@@ -19,52 +19,52 @@ const initialState: CellsState = {
   data: {},
 };
 
-const reducer = produce((state: CellsState = initialState, action: Action) => {
-  switch (action.type) {
-    case ActionType.UPDATE_CELL:
-      const { id, content } = action.payload;
-      state.data[id].content = content;
-      return;
+const reducer = produce(
+  (state: CellsState = initialState, action: Action): CellsState => {
+    switch (action.type) {
+      case ActionType.UPDATE_CELL:
+        const { id, content } = action.payload;
+        state.data[id].content = content;
+        return state;
 
-    case ActionType.DELETE_CELL:
-      state.order = state.order.filter((id) => id !== action.payload);
-      delete state.data[action.payload];
-      return;
+      case ActionType.DELETE_CELL:
+        state.order = state.order.filter((id) => id !== action.payload);
+        delete state.data[action.payload];
+        return state;
 
-    case ActionType.INSERT_CELL_BEFORE:
-      const cell = {
-        content: '',
-        type: action.payload.type,
-        id: randomId(),
-      };
-      state.data[cell.id] = cell;
+      case ActionType.INSERT_CELL_BEFORE:
+        const cell = {
+          content: '',
+          type: action.payload.type,
+          id: randomId(),
+        };
+        state.data[cell.id] = cell;
 
-      const foundIndex = state.order.findIndex(
-        (id) => id === action.payload.id
-      );
+        const foundIndex = state.order.findIndex(
+          (id) => id === action.payload.id
+        );
 
-      if (foundIndex < 0) {
-        state.order.push(cell.id);
-        return;
-      } else {
+        if (foundIndex < 0) return state;
+
         state.order.splice(foundIndex, 0, cell.id);
-      }
-      return;
+        return state;
 
-    case ActionType.MOVE_CELL:
-      const { direction } = action.payload;
-      const index = state.order.findIndex((id) => id === action.payload.id);
-      const newIndex = direction === 'up' ? index - 1 : index + 1;
-      if (newIndex < 0 || newIndex > state.order?.length - 1) return;
+      case ActionType.MOVE_CELL:
+        const { direction } = action.payload;
+        const index = state.order.findIndex((id) => id === action.payload.id);
+        const newIndex = direction === 'up' ? index - 1 : index + 1;
 
-      state.order[index] = state.order[newIndex];
-      state.order[newIndex] = action.payload.id;
-      return;
+        if (newIndex < 0 || newIndex > state.order?.length - 1) return state;
 
-    default:
-      return state;
+        state.order[index] = state.order[newIndex];
+        state.order[newIndex] = action.payload.id;
+        return state;
+
+      default:
+        return state;
+    }
   }
-});
+);
 const randomId = () => {
   // bas 36 means letters and numbers
   return Math.random().toString(36).substr(2, 5);
