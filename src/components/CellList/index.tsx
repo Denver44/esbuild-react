b137781e90b47a1269044c59f1dac78059bdca87
Denver <70090559+Denver44@users.noreply.github.com>
2022-02-19 01:14:06 +0530

    Manula testing of orderList done

--- src/components/CellList/index.tsx
+++ src/components/CellList/index.tsx
@@ -1,9 +1,16 @@
 import { useTypedSelector } from '../../hook/used-typed-selector';
+import CellListItem from '../CellListItem';
 
-const CellList = () => {
-  useTypedSelector((state) => state);
+const CellList: React.FC = () => {
+  const cells = useTypedSelector(({ cells }) =>
+    cells?.order.map((id) => cells.data[id])
+  );
 
-  return <div>CellList</div>;
+  const renderedCells = cells?.map((cell) => (
+    <CellListItem key={cell.id} cell={cell} />
+  ));
+
+  return <div>{renderedCells}</div>;
 };
 
 export default CellList;
