bc0b50d9d8e42bfeeaa93567ccb67e8903a7d5f2
Denver <70090559+Denver44@users.noreply.github.com>
2022-02-19 11:26:57 +0530

    Adding new cell done

---
+++ src/components/AddCell/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useAction } from '../../hook/use-actions';
+
+interface AddCellProps {
+  nextCellId: string;
+}
+
+const AddCell: React.FC<AddCellProps> = ({ nextCellId }) => {
+  const { insertCellBefore } = useAction();
+  return (
+    <div>
+      <button onClick={() => insertCellBefore(nextCellId, 'code')}>
+        + Code
+      </button>
+      <button onClick={() => insertCellBefore(nextCellId, 'text')}>
+        + Text
+      </button>
+    </div>
+  );
+};
+
+export default AddCell;
